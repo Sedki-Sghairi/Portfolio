7fd8df620bca723f95562409530f071b13808258
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-14 13:06:06 +0400

    services component

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,26 @@
-import React from "react"
-import Title from "./Title"
-import services from "../constants/services"
+import React from 'react';
+import Title from './Title';
+import services from '../constants/services';
+import { FaFontAwesomeLogoFull } from 'react-icons/fa';
 const Services = () => {
-  return <h2>services component</h2>
-}
+	return (
+		<section className="section bg-grey">
+			<Title title="services" />
+			<div className="section-center services-center">
+				{services.map((x) => {
+					const { id, icon, title, text } = x;
+					return (
+						<article key={id} className="service">
+							{icon}
+							<h4>{title}</h4>
+							<div className="underline" />
+							<p>{text}</p>
+						</article>
+					);
+				})}
+			</div>
+		</section>
+	);
+};
 
-export default Services
+export default Services;
