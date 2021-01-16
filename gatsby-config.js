fd75565607a0695efb08278f798d01d52ef4b834
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-16 18:11:49 +0400

    get all projects and pass featured onces to project component

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
 			options: {
 				apiURL: `http://localhost:1337`,
 				queryLimit: 1000, // Default to 100
-				contentTypes: [ `jobs` ],
-				// , `projects`, `blogs`
+				contentTypes: [ `jobs`, `projects` ],
+				// , , `blogs`
 				singleType: `about`
 				// singleTypes: [],
 			}
