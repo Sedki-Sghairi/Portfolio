c070eec56f4494b91ef73be536e33f9668829f3c
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-01-18 01:48:43 +0400

    error page and sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,19 @@
 import React from "react"
-// import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
+import { useState } from "react"
 const Layout = ({ children, light }) => {
-  
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
   return (
     <>
-      <Navbar light={light}/>
+      <Navbar light={light} toggle={toggle} />
+      <Sidebar toggle={toggle} isOpen={isOpen}/>
       {children}
       <Footer/>
     </>
   )
 }
-
+ 
 export default Layout
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from '../assets/log0.png';
 import { FaAlignRight } from 'react-icons/fa';
 import PageLinks from '../constants/links';
-const Navbar = ({ light }) => {
+const Navbar = ({ light, toggle }) => {
 	return (
 		<nav className="navbar">
 			<div className="nav-center">
 				<div className="nav-header">
 					<img src={logo} alt="logo" />
-					<button type="button" className="toggle-btn">
+					<button type="button" className="toggle-btn" onClick={toggle}>
 						<FaAlignRight />
 					</button>
 				</div>
