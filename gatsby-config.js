c6065c263f2650abb4fc5f7f1e067652f53e8e75
Sedki-Sghairi <sedkisghairi3@gmail.com>
2021-02-22 18:22:58 +0400

    configure google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,31 +14,22 @@ module.exports = {
 		`gatsby-plugin-sitemap`,
 		`gatsby-plugin-scroll-reveal`,
 		{
-			resolve: 'gatsby-plugin-google-tagmanager',
+			resolve: `gatsby-plugin-google-gtag`,
 			options: {
-				id: 'GTM-MVCCCTK',
-
-				// Include GTM in development.
-				//
-				// Defaults to false meaning GTM will only be loaded in production.
-				includeInDevelopment: false,
-
-				// datalayer to be set before GTM is loaded
-				// should be an object or a function that is executed in the browser
-				//
-				// Defaults to null
-				defaultDataLayer: { platform: 'gatsby' },
-
-				// Specify optional GTM environment details.
-				gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-				gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-				dataLayerName: 'YOUR_DATA_LAYER_NAME',
-
-				// Name of the event that is triggered
-				// on every Gatsby route change.
-				//
-				// Defaults to gatsby-route-change
-				routeChangeEventName: 'YOUR_ROUTE_CHANGE_EVENT_NAME'
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [ 'G-8XW63WY124' ],
+				// This object gets passed directly to the gtag config command
+				// This config will be shared across all trackingIds
+				// gtagConfig: {
+				// 	optimize_id: 'OPT_CONTAINER_ID',
+				// 	anonymize_ip: true,
+				// 	cookie_expires: 0
+				// },
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: true
+				}
 			}
 		},
 		{
